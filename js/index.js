721799d72321d2e2c4277dcd88ec0da9a245a651
Divyam Kumar <divyamkumarsingh@gmail.com>
2021-01-06 16:23:02 +0530

    added ng repeat and nested ng repeat

--- js/index.js
+++ js/index.js
@@ -6,4 +6,49 @@ app.controller("myController", ($scope)=>{
         name : "Teaching",
         path : "img/yt.svg"
     }
+
+    // It seems that ng repeat only works on arrays
+    let employees = [
+        {name: "Divyam", Age : "23", Gender: "Male", Role: "Developer"},
+        {name: "Abhishek", Age : "20", Gender: "Male", Role: "CEO"},
+        {name: "Punnet", Age : "23", Gender: "Male", Role: "Developer"},
+        {name: "Akshat", Age : "23", Gender: "Male", Role: "C"}
+    ];
+    $scope.employee = employees;
+
+    //Creating model for nested ng-repeat
+
+    let countries = [
+        {
+            name : "USA",
+            Cities : [
+                {name : "Laos Angeles"},
+                {name : "Chicago"},
+                {name : "Houston"},
+            ]
+        },
+        {
+            name : "India",
+            Cities : [
+                {name : "Patna"},
+                {name : "Ranchi"},
+                {name : "Delhi"}
+            ]
+        },
+        {
+            name : "Japan",
+            Cities: [
+                {name : "Fukuoka"},
+                {name : "Tokyo"},
+                {name: "Nara"}
+            ]
+
+
+        }
+
+    ];
+    $scope.countries = countries;
+    //model creation for nested ng-if complete
+
+
 })
